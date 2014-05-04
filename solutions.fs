3ad0403bd51c8715e667245063ef6c5f4d7f8f62
harrypower <philipkingsmith@gmail.com>
2014-05-03 20:50:07 -0600

    separated solution words from piece and board words

---
+++ solutions.fs
@@ -0,0 +1,55 @@
+include pieces.fs
+
+\ *************
+\ these structures and words are an attempt to understand total rotational combinations
+\ *************
+x-count y-count * z-count * rotations * constant total-possibilitys
+variable total-solutions 0 total-solutions !
+
+loc%
+    cell% field rot#
+end-struct solution-list%
+
+create thesolutions
+thesolutions solution-list% %size total-possibilitys * dup allot erase
+
+
+: solutions! ( nx ny nz nrot nindex -- )
+    solution-list% %size * thesolutions + dup
+    rot# rot swap ! dup
+    z rot swap ! dup
+    y rot swap !
+    x ! ;
+
+: solutions@ ( nindex -- nx ny nz nrot )
+    solution-list% %size * thesolutions + dup
+    x @ swap dup
+    y @ swap dup
+    z @ swap
+    rot# @ ;
+
+: do-inner-solutions { nindex nx ny nz -- nindex1 }
+
+    rotations 0
+    ?DO
+	i nx ny nz place-piece? false =
+	if
+	    nx ny nz i nindex solutions!
+	    nindex 1 + to nindex
+	then
+    LOOP
+    nindex ;
+
+: make-solutions-list ( -- ) \ populate thesolutions list
+    clear-board 0
+    x-count 0 ?DO
+	y-count 0 ?DO
+	    z-count 0 ?DO
+		k j i do-inner-solutions
+	    LOOP
+	LOOP
+    LOOP total-solutions ! ;
+
+\ ************
+\ These next words are about solving one path of the puzzle to see how it could be done
+\ ************
