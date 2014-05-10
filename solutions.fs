973a044c661ef4da0f92d0ecb4c2c3de7968c3d6
harrypower <philipkingsmith@gmail.com>
2014-05-09 19:54:40 -0600

    Back trace method is not easy to do or simply will not work so groupings of rotational combinations is the next method

--- solutions.fs
+++ solutions.fs
@@ -54,11 +54,16 @@ thesolutions solution-list% %size total-possibilitys * dup allot erase
 \ ************
 \ These next words are about solving one path of the puzzle to see how it could be done
 \ ************
+\ A flaw with this method is back tracing for solutions means you need to remember multipul failed
+\ piece placement to find the combinations that work. This is not a good way to do it!
 
 25 constant total-pieces \ this is the amount of pieces needed to be placed to solve puzzle
 0 value working-piece  \ this will be the piece that is being worked on now
 create thepieces-solution  \ a table for working on the final solutions.  Will contain a list of total-pieces as that is the amount needed to solve this puzzle
 thepieces-solution solution-list% %size total-pieces * dup allot erase
+create thelastpieces-solution  \ a table that will be the working copy of the final solutions .
+thelastpieces-solution solution-list% %size total-pieces * dup allot erase
+
 solution-list%
     cell% field thepiece#
 end-struct working-solution-list% 
@@ -93,23 +98,34 @@ thelast-solution working-solution-list% %size dup allot erase
     thelast-solution rot# @
     thelast-solution thepiece# @ ;
 
+: skiptest ( nx ny nz nrot -- nflag ) \ nflag is true only if the skip should be performed
+    thelast-solution thepiece# @ total-pieces = 
+    if
+	2drop 2drop false
+    else
+	thelast-solution rot# @ = swap
+	thelast-solution z @ = and swap
+	thelast-solution y @ = and swap
+	thelast-solution x @ = and
+	working-piece thelast-solution thepiece# @ = and 
+    then ;
+
 : do-rotation-placement { nx ny nz -- }
     working-piece total-pieces <  
     if
 	rotations 0 ?DO
-	    i nx ny nz place-piece? false =
+	    i nx ny nz place-piece? \ false =
+	    nx ny nz i skiptest or false = 
 	    if
 		working-piece 1 + i nx ny nz ponboard
 		nx ny nz i working-piece thepieces-solution!
-		nx ny nz i working-piece thelast-solution!
 		working-piece 1 + to working-piece
 		LEAVE
 	    then
 	LOOP
-    then
-;
+    then ;
 
-: find-first-solution ( -- )
+: fill-holes ( -- )
     x-count 0 ?DO
 	y-count 0 ?DO
 	    z-count 0 ?DO
@@ -119,8 +135,7 @@ thelast-solution working-solution-list% %size dup allot erase
 		then
 	    LOOP
 	LOOP
-    LOOP
-;
+    LOOP ;
 
 : repopulate-board ( -- )
     clear-board
@@ -128,17 +143,30 @@ thelast-solution working-solution-list% %size dup allot erase
 	i 1 + i thepieces-solution@ swap 2swap rot ponboard 
     LOOP ;
 
-: fill-holes ( -- )
-    thelast-solution thepiece# @ to working-piece
-    \ need to iterate through the display for holes and try to solve them 
-    x-count 0 ?DO
-	y-count 0 ?DO
-	    z-count 0 ?DO
-		k j i piece-there? false =
-		if
-		    k j i do-rotation-placement
-		then
-	    LOOP
-	LOOP
-    LOOP ;
+: find-many-solutions ( -- )
+    clear-board
+    0 to working-piece
+    total-pieces thelast-solution thepiece# ! \ to ensure skip test is not done for first solution finding
+    fill-holes 
+    working-piece 1 - thepieces-solution@ working-piece 1 - thelast-solution!
+    begin
+	repopulate-board
+	working-piece 1 - to working-piece
+	fill-holes
+	working-piece total-pieces >= 
+	if
+	    true
+	else
+	    working-piece 1 - thepieces-solution@ working-piece 1 - thelast-solution! 
+	    false
+	then
+	working-piece . thelast-solution@ . . . . . cr 500 ms
+    until
+;
+
+\ ****************************
+\ These next words will take the data table made from the total rotatianl combinations and
+\ make groupings of rotational combinations.  This would allow groups of 2 , 3 , 4  or more so then
+\ it is a matter of grouping groups that work together to solve the final puzzle
+\ ****************************
 
