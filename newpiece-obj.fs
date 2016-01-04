c6115fc61557057424246a3b196bb2ffa8849c67
Philip Smith <philipkingsmith@gmail.com>
2016-01-04 15:27:32 -0700

    base shapes defined

--- newpiece-obj.fs
+++ newpiece-obj.fs
@@ -33,20 +33,42 @@ object class
   ;m method bshape@
   public
   m: ( piece -- )
-    0 0 0 base-shapes a 0 this bshape!
+    0 0 0 base-shapes a 0 this bshape! \ first shape
     1 0 0 base-shapes b 0 this bshape!
     2 0 0 base-shapes c 0 this bshape!
     2 0 1 base-shapes d 0 this bshape!
     3 0 1 base-shapes e 0 this bshape!
+    0 0 1 base-shapes a 1 this bshape! \ second shape
+    1 0 1 base-shapes b 1 this bshape!
+    2 0 1 base-shapes c 1 this bshape!
+    2 0 0 base-shapes d 1 this bshape!
+    3 0 0 base-shapes e 1 this bshape!
+    0 0 0 base-shapes a 2 this bshape! \ third shape
+    1 0 0 base-shapes b 2 this bshape!
+    1 0 1 base-shapes c 2 this bshape!
+    2 0 1 base-shapes d 2 this bshape!
+    3 0 1 base-shapes e 2 this bshape!
+    0 0 1 base-shapes a 3 this bshape! \ fourth shape
+    1 0 1 base-shapes b 3 this bshape!
+    1 0 0 base-shapes c 3 this bshape!
+    2 0 0 base-shapes d 3 this bshape!
+    3 0 0 base-shapes e 3 this bshape!
   ;m overrides construct
   m: ( piece -- )
-    base-shapes e 0 this bshape@
+    base-shapes e 3 this bshape@ . . . cr
+    base-shapes d 2 this bshape@ . . . cr
   ;m method testing
 end-class piece
 
 piece class
 
+  m: ( piece -- )
+    this [parent] construct
+  ;m overrides construct
 end-class board
 
 piece heap-new constant ptest
 ptest testing
+
+board heap-new constant btest
+btest testing
