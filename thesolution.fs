4d2a7e92a667ef164415c41c44ba403509767493
Philip Smith <philipkingsmith@gmail.com>
2016-10-04 11:36:13 -0600

    started 2 piece object

--- thesolution.fs
+++ thesolution.fs
@@ -1,1 +1,8 @@
-require thepiece.fs
+require c:\users\philip\documents\github\cubesolve\thepiece.fs
+
+object class
+  destruction implementation
+  inst-value piecea
+  inst-value pieceb
+  
+end-class 2pieces
