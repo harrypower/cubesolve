2d55891d2f0ec70f0b189bbb05b1e6ee64383626
Philip Smith <philipkingsmith@gmail.com>
2016-10-05 01:36:09 -0600

    method building and testing

--- thesolution.fs
+++ thesolution.fs
@@ -12,22 +12,39 @@ object class
   inst-value piecelistsize
   cell% inst-var 2piece-test
   960 constant pindex-max
+  inst-value piecea-object-addr
 
+  \ protected \ ********************************************************************************************************
+  m: ( np ni 2pieces -- ) \ store pieceb number at index
+    piecepair% %size * addrpiecelist + !
+  ;m method npieceb!
+  m: ( ni 2pieces -- np ) \ retrieve pieceb number from index
+    piecepair% %size * addrpiecelist + @
+  ;m method npieceb@
+  public \ ***********************************************************************************************************
   m: ( n 2pieces -- ) \ construct a piece pair list
-    \ make room to store pindex-max numbers for the pieceb value and store n into piecea then find all the pieceb values from piecea
-    2piece-test 2piece-test @ <> \ to allocate this on the heap only once at fist construct execution time
-    if piecepair% %alignment piecepair% %size pindex-max * %allocate throw [to-inst] addrpiecelist then
-    [to-inst] piecea
+    \ make room to store pindex-max numbers for the pieceb value and store n into piecea then find all the pieceb values for piecea
+    2piece-test 2piece-test @ <> \ to allocate this on the heap only once at fist construct execution time and after destruct method
+    if
+      piecepair% %alignment piecepair% %size pindex-max * %allocate throw [to-inst] addrpiecelist
+      piece heap-new [to-inst] piecea-object-addr
+    then
+    addrpiecelist pindex-max erase
+    0 [to-inst] piecelistsize
+    dup [to-inst] piecea
+    piecea-object-addr newpiece!
     2piece-test 2piece-test ! \ set test now that construct has run once
   ;m overrides construct
   m: ( 2pieces -- ) \ to release memory of this pair list
     addrpiecelist free throw
     0 [to-inst] addrpiecelist
     0 2piece-test !
+    piecea-object-addr destruct
+    piecea-object-addr free throw
   ;m overrides destruct
   m: ( nindex 2pieces -- npiecea npieceb ) \ for the present nindex return npiecea and npieceb values to stack
-  ;m method npair!
-
+    this [current] npieceb@ piecea swap
+  ;m method npair@
   m: ( 2pieces -- nsize )
     piecelistsize
   ;m method piecelistsize@
@@ -39,3 +56,9 @@ object class
     ." 2piece-test address " 2piece-test . cr
   ;m overrides print
 end-class 2pieces
+
+59 2pieces dict-new constant a
+a print cr
+5 10 a npieceb!
+10 a npieceb@ . cr
+10 a npair@ . . cr
