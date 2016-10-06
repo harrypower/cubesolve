e4cb6462b0a8f7b882545e8875902eb605299905
Philip Smith <philipkingsmith@gmail.com>
2016-10-05 18:24:01 -0600

    started the total pair calculation in 2piece object

--- thesolution.fs
+++ thesolution.fs
@@ -4,17 +4,24 @@ object class
   destruction implementation
 
   struct
-      cell% field pieceb
+    cell% field pieceb
   end-struct piecepair%
+  struct
+    cell% field p1
+    cell% field p2
+  end-struct totalpairs%
 
   inst-value piecea
   inst-value addrpiecelist
   inst-value piecelistsize
   cell% inst-var 2piece-test
   960 constant pindex-max
+  767000 constant pairindex-max
   inst-value piecea-object-addr
+  inst-value totalpairlist
+  inst-value pairlistsize
 
-  \ protected \ ********************************************************************************************************
+  protected \ ********************************************************************************************************
   m: ( np ni 2pieces -- ) \ store pieceb number at index
     piecepair% %size * addrpiecelist + pieceb !
   ;m method npieceb!
@@ -39,9 +46,12 @@ object class
     if
       piecepair% %alignment piecepair% %size pindex-max * %allocate throw [to-inst] addrpiecelist
       piece heap-new [to-inst] piecea-object-addr
+      totalpairs% %alignment totalpairs% %size pairindex-max * %allocate throw [to-inst] totalpairlist
     then
     addrpiecelist pindex-max erase
+    totalpairlist pairindex-max erase
     0 [to-inst] piecelistsize
+    0 [to-inst] pairlistsize
     dup [to-inst] piecea
     piecea-object-addr newpiece!
     this [current] populatepieceb
@@ -49,8 +59,10 @@ object class
   ;m overrides construct
   m: ( 2pieces -- ) \ to release memory of this pair list
     addrpiecelist free throw
+    totalpairlist free throw
     0 [to-inst] addrpiecelist
     0 [to-inst] piecelistsize
+    0 [to-inst] pairlistsize
     0 2piece-test !
     piecea-object-addr destruct
     piecea-object-addr free throw
@@ -71,10 +83,15 @@ object class
 end-class 2pieces
 
 0 2pieces dict-new constant a
-a print cr
-0 a npair@ . . cr
-1 a npair@ . . cr
-2 a npair@ . . cr
+0 value totalpairs
 
-: testsize a pairlistsize@ 0 do i a npair@ swap . . cr loop ;
-testsize
+: findtotalpairs ( -- )
+  960 0 do
+    i a construct
+    a pairlistsize@ totalpairs + to totalpairs
+  loop ;
+utime
+findtotalpairs
+utime
+totalpairs . space ." the total pairs!" cr
+2swap d- d. cr
