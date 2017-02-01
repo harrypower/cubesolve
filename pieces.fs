fb469329de63eb0e5ea68f9917af4af8a2958204
Philip Smith <philipkingsmith@gmail.com>
2017-01-31 19:22:45 -0700

    pieces object will contain the puzzle pieces defined

--- pieces.fs
+++ pieces.fs
@@ -26,4 +26,58 @@ end-class puzzle-board
 
 create puzzle-board-dimensions puzzle-board dict-new drop
 
+struct-base class
+  protected
+  struct
+    char% field voxel-l
+    char% field voxel-w
+    char% field voxel-h
+  end-struct temp-voxel%
+  cell% inst-var piece-list
+  cell% inst-var voxel-list
+  public
+  m: ( pieces -- ) \ constructor
+    construct? construct? @ = if
+    \ stuff here to deallocate memory
+    \ destruct all voxel list in each piece from piece list
+    \ destruct piece list itself
+    \ destruct the voxel-list itself
+    \ 0 construct to the parent construct
+    else
+      double-linked-list heap-new piece-list !
+      double-linked-list heap-new voxel-list !
+      1 temp-voxel% this [parent] construct
+    then ;m overrides construct
+  m: ( pieces -- ) \ add voxel list to the current piece in the piece-list
+    voxel-list cell
+    piece-list @ ll!
+    double-linked-list heap-new voxel-list !
+  ;m method define
+  m: ( pieces -- uquantity ) \ return quantity of pieces
+    piece-list @ ll-size@ ;m method piece-quantity
+  m: ( ul uw uh pieces -- ) \ add voxel to voxel-list
+    0 this :: voxel-h c!
+    0 this :: voxel-w c!
+    0 this :: voxel-l c!
+    0 this :: size @ voxel-list @ ll!
+  ;m method add-voxel
+  m: ( uindex pieces -- ul uw uh ) \ retrieve voxel from piece uindex
+    piece-list @ ll-set-start
+    0 ?do piece-list @ ll> abort" Trying to get a piece that is not there!" loop
+    piece-list @ ll@ drop @ ll@ drop
+    dup >r voxel-l c@
+    r@ voxel-w c@
+    r> voxel-h c@ ;m method seeavoxel
+  m: ( unindex pieces -- ) \ move voxel list pointer to next voxel from piece uindex
+    piece-list @ ll-set-start
+    0 ?do piece-list @ ll> abort" Trying to get a piece that is not there!" loop
+    piece-list @ ll@ drop @ ll> if piece-list @ ll@ drop @ ll-set-start then ;m method nextvoxel
+  m: ( uindex pieces -- usize ) \ return voxel quantity from piece uindex
+    piece-list @ ll-set-start
+    0 ?do piece-list @ ll> abort" Trying to get a piece that is not there!" loop
+    piece-list @ ll@ drop @ ll-size@ ;m method voxel-quantity
+end-class pieces
+
+create puzzle-pieces pieces dict-new drop
+
 include ./puzzle.def
