339aec080ffa53fe474f99658fadc9ed967b8203
Philip Smith <philipkingsmith@gmail.com>
2017-04-11 12:41:04 -0600

    added construct and storage methods to array

--- piece-array.fs
+++ piece-array.fs
@@ -1,4 +1,5 @@
 require ./Gforth-Objects/objects.fs
+require ./newpieces.fs
 
 [ifundef] destruction
   interface
@@ -14,15 +15,29 @@ object class
   struct
     cell% field piece-cell
   end-struct piece-cell%
+  m: ( npiece nindex piece-array -- ) \ store piece object into array
+    piece-cell% %size * pieces-array-start @ + piece-cell !
+  ;m method npiece!
+
   public
   m: ( npieces piece-array -- ) \ construct the array from the contents of npieces!  Note the size is fixed at construct time!
+    { npieces } npieces pieces-quantity@ dup pieces-array-quantity !
+    piece-cell% %size * allocate throw pieces-array-start !
+    pieces-array-quantity @ 0 do?
+      i npieces get-a-piece
+      piece heap-new dup i this npiece!
+      [bind] piece copy
+    loop
   ;m overrides construct
 
   m: ( piece-array -- ) \ destruct the memory used!
+    \ free the piece's in the array
+    \ free the array itself
+    \ clean up other stuff
   ;m overrides destruct
 
   m: ( nindex piece-array -- npiece) \ retrieve npiece from array at nindex location
-  ;m method npiece@
+    piece-cell% %size * pieces-array-start @ + piece-cell @ ;m method npiece@
 
   m: ( piece-array -- nquantity ) \ return the array size
     pieces-array-quantity @ ;m method quantity@
