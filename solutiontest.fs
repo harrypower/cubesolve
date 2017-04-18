1dbb7322b5e5009471db16b48dd3cdbcac76a48d
Philip Smith <philipkingsmith@gmail.com>
2017-04-18 17:52:10 -0600

    update before change to piece-array

--- solutiontest.fs
+++ solutiontest.fs
@@ -5,6 +5,9 @@ require ./group-list.fs
 0 puzzle-pieces make-all-pieces heap-new constant map \ this object is used to make reference lists from start pieces
 constant ref-piece-list \ this is the reference list of piece created above
 ref-piece-list piece-array heap-new constant ref-piece-array \ this object takes reference list and makes a reference array of list for indexing faster
+
+
+\\\
 2 group-list heap-new constant piece-pair-list \ this object will contain the reference pair list of all pieces
 
 : makepairs ( -- )
