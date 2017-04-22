9b405d32f73c8b3ffe1e2f9846bc9191ab14edcf
Philip Smith <philipkingsmith@gmail.com>
2017-04-22 17:30:23 -0600

    removed definition of cell-array

--- array-object.fs
+++ array-object.fs
@@ -6,54 +6,6 @@ require ./Gforth-Objects/objects.fs
   end-interface destruction
 [endif]
 
-object class \ this is just a 2d cell array
-  destruction implementation
-  protected
-  cell% inst-var storage-location
-  cell% inst-var records
-  cell% inst-var fields
-  cell% inst-var fields*
-  m: ( ufields uindex cell-array -- uaddr )
-    fields* @ * swap cell * + storage-location @ +
-  ;m method calc-array-addr
-  public
-  m: ( ufields urecords cell-array -- )
-    2dup records ! fields !
-    * cell * allocate throw storage-location !
-    fields @ cell * fields* !
-  ;m overrides construct
-
-  m: ( cell-array -- )
-    storage-location @ free throw
-    0 records !
-    0 fields !
-    0 fields* !
-  ;m overrides destruct
-
-  m: ( ufield uindex cell-array -- nvalue )
-    this calc-array-addr @ ;m method ncell-array@
-
-  m: ( nvalue ufield uindex cell-array -- )
-    this calc-array-addr ! ;m method ncell-array!
-
-  m: ( nvalue0 ... nvaluex uindex cell-array -- ) \ this stores one row or index of values at a time so there needs to be a complete row of values
-    0 swap this calc-array-addr { addr }
-    -1 fields @ 1 - ?do
-      addr cell i * + !
-    1 -loop
-  ;m method xcell-array!
-
-  m: ( uindex cell-array -- nvalue0 ... nvaluex ufield )
-    0 swap this calc-array-addr { addr }
-    fields @ 0 ?do
-      addr cell i * + @
-    loop fields @
-  ;m method xcell-array@
-
-  m: ( cell-array -- ufields urecords )
-    fields @ records @ ;m method size@
-end-class cell-array
-
 object class \ this is a multi dimension cell array
   destruction implementation
   protected
