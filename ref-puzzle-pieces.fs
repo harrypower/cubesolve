c4f08c4bf3f29ff2e1d7b7d9677936b315cbdd14
Philip Smith <philipkingsmith@gmail.com>
2017-04-26 17:56:32 -0600

    almost ready to test hole-array-piece-list

--- ref-puzzle-pieces.fs
+++ ref-puzzle-pieces.fs
@@ -19,12 +19,33 @@ object class
   destruction implementation
   protected
   cell% inst-var hole-array     \ address of the array that holds the hole reference lists
-  cell% inst-var hole-size      \ the size of the hole-array in terms of how many holes or voxel
   cell% inst-var a-ref-piece-array  \ the upiece-array object passed to constructor used as the reference
   cell% inst-var the-puzzle-board   \ the board oject that is passed to construct used to size the hole-array and hole-size values
 
-  m: ( hole-array-piece-list -- ) \ populate the hole-array with lists of reference pieces that fit in each hole
+  m: ( hole-array-piece-list -- uholex uholey uholez ) \ return hole address max values
     the-puzzle-board @ [bind] board get-board-dims
+  ;m method hole-address@
+
+  m: ( uref-piece uholex uholey uholez hole-array-piece-list -- ) \ store uref-piece into hole list at uholex uholey uholez address
+    hole-array @ [bind] multi-cell-array cell-array@
+    [bind] double-linked-list ll!
+  ;m method next-piece-in-hole!
+
+  m: ( uholex uholey uholez hole-array-piece-list -- )
+    0 { uholex uholey uholez upiece }
+    a-ref-piece-array @ [bind] piece-array quantity@ 0 ?do
+      i a-ref-piece-array @ [bind] piece-array upiece@ to upiece
+      upiece [bind] piece voxel-quantity@ 0 ?do
+        i upiece [bind] piece get-voxel
+        uholez = swap uholey = and swap uholex = and true = if
+          j uholex uholey uholez this next-piece-in-hole!
+        then
+      loop
+    loop
+  ;m method do-populate-holes
+
+  m: ( hole-array-piece-list -- ) \ populate the hole-array with lists of reference pieces that fit in each hole
+    this hole-address@
     0 ?do \ z
       0 ?do \ y
         0 ?do \ x
@@ -33,16 +54,21 @@ object class
       loop
     loop
   ;m method populate-holes
+
   public
   m: ( upiece-array uboard hole-array-piece-list -- ) \ constructor
     \ takes upiece-array that should contain the reference pieces and oranizes them for hole indexing or voxel indexing
     \ uboard should be puzzle-board that contains the size of the current puzzle being solved for
     the-puzzle-board !
     a-ref-piece-array !
-    the-puzzle-board @ [bind] board get-board-dims * * hole-size !
-    hole-size @ 1 multi-cell-array heap-new hole-array !
-    hole-size @ 0 ?do
-      double-linked-list heap-new i hole-array @ [bind] multi-cell-array cell-array!
+    this hole-address@ 3 multi-cell-array heap-new hole-array !
+    this hole-address@
+    0 ?do \ z
+      0 ?do \ y
+        0 ?do \ x
+          double-linked-list heap-new i j k hole-array @ [bind] multi-cell-array cell-array!
+        loop
+      loop
     loop
     this populate-holes
   ;m overrides construct
@@ -50,19 +76,22 @@ object class
   m: ( hole-array-piece-list -- ) \ destructor
   ;m overrides destruct
 
-  m: ( uhole-index hole-array-piece-list -- ux uy uz )
+  m: ( uholex uholey uholez hole-array-piece-list -- uref-piece nflag )
+    \ uholex uholey uholez is the hole address identified from puzzle-board
+    \ uref-piece is the next reference piece in the list that fits in that hole address
+    \ nflag is false normaly after upiece retrieval
+    \ nflag is true when for givin hole address the piece list is at the end
+    \ note when nflag is true the piece list at that hole address will reset to begining of list
+    hole-array @ [bind] multi-cell-array cell-array@
+    dup rot swap [bind] double-linked-list ll@>
+    dup true = if swap [bind] double-linked-list ll-set-start else swap drop then
+  ;m method next-ref-piece-in-hole@
 
-  ;m method hole-location@
-
-  m: ( uhole-index hole-array-piece-list -- upiece nflag )
-    \ uhole-index is the reference index indicating all the holes or voxels identified from puzzle-board
-
-  ;m method next-piece-in-hole@
-
-  m: ( uhole-index hole-array-piece-list -- uhole-list-quantity )
-    hole-array @ [bind] multi-cell-array cell-array-dimensions@ drop
+  m: ( uholex uholey uholez hole-array-piece-list -- uhole-list-quantity ) \ returns the quantity of pieces in a given hole
+    hole-array @ [bind] multi-cell-array cell-array@
+    [bind] double-linked-list ll-size@
   ;m method hole-list-quantity@
 
-  m: ( hole-array-piece-list -- uhole-size )
-    hole-size @ ;m method hole-amounts@
+  m: ( hole-array-piece-list -- uholex uholey uholez )  \ returns the total hole addresses for this reference puzzle passed to construct
+    this hole-address@ ;m method hole-max-address@
 end-class hole-array-piece-list
