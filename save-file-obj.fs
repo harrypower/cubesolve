639c6f78c56333c428dee7a30102a44f1b451b71
Philip Smith <philipkingsmith@gmail.com>
2017-06-27 01:28:05 -0600

    retrieve-data method working

--- save-file-obj.fs
+++ save-file-obj.fs
@@ -17,6 +17,7 @@ object class
   inst-value save-fid
   inst-value save$
   inst-value working$
+  inst-value parse$
   inst-value separate$
   public
   m: ( save -- ) \ create string of the data to be saved
@@ -30,6 +31,26 @@ object class
   m: ( save -- ) \ save the save data
     working$ @$ save-fid write-file throw
   ;m method do-save-data
+  m: ( save -- caddr u nflag ) \ caddr u contains the data found before the seperator and nflag is true
+    \ caddr u contains an empty string if the seperator is not found and nflag is false
+    \ working$ is adjusted to have the remander of the string after seperator removed and the part before seperator is also removed
+    \ note working$ could be an empty string after this process ends !
+    separate$ @$ working$ [bind] string split$ ( caddrf uf caddrl ul nflag )
+    >r 2swap parse$ [bind] string !$ working$ [bind] string !$ parse$ [bind] string @$ r>
+    \ working$ [bind] string @$ dump cr ." ^ (do-parse-data)" cr
+  ;m method (do-parse-data)
+  m: ( save -- ) \ parse the data from working$ buffer
+    try
+      this (do-parse-data) false = if true throw then 2drop
+      begin
+        this (do-parse-data) false = if true throw then \ 2dup dump cr
+        save$ [bind] strings !$x
+        working$ [bind] string len$ 0 = \ .s cr working$ [bind] string @$ dump cr
+      until
+      false
+    restore if save$ [bind] strings destruct then
+    endtry
+  ;m method do-parse-data
   m: ( nstrings save -- ) \ save the data to restart later
     \ nstrings is the strings object that contains strings of data to be put into a save file
     \ note the nstrings strings object is copied into this object and not altered in any way
@@ -55,13 +76,25 @@ object class
     then
   ;m method save-data
   m: ( save -- nstrings ) \ retrieve the saved data from file
-
+    save$ [bind] strings destruct
+    cr ." Enter the path and file name of data file to retrieve > "
+    save-file 250 accept [to-inst] save-file-size
+    save-file save-file-size file-status swap drop
+    false = if \ directory and file name there so retrieve the data
+      save-file save-file-size slurp-file
+      working$ [bind] string !$
+      this do-parse-data
+    else
+      cr ." The path and file name does not exist!"
+    then
+    save$
   ;m method retrieve-data
   m: ( save -- ) \ constructor
     250 allocate throw [to-inst] save-file
     strings heap-new [to-inst] save$
     string heap-new [to-inst] working$
     string heap-new [to-inst] separate$
+    string heap-new [to-inst] parse$
     s" |||" separate$ [bind] string !$
   ;m overrides construct
   m: ( save -- ) \ destructor
@@ -84,4 +117,15 @@ s" 923" dataset !$x
 s" moredata" dataset !$x
 s" 27" dataset !$x
 
-dataset testsave save-data
+ dataset testsave save-data
+
+ testsave retrieve-data
+
+\\\
+string heap-new constant what
+s" 27|||th" what !$
+what @$ dump cr ." ^ that is what before" cr
+s" |||" what split$ .
+dump cr ." ^ that is first dump" cr
+
+dump cr ." ^ that is second dump" cr
